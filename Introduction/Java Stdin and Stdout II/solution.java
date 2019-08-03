e410301512113666d7598e3f052243cf2d8e6621
trushakarandikar <trushakarandikar@gmail.com>
2019-08-03 16:06:04 +0530

    Create solution.java

---
+++ Introduction/Java Stdin and Stdout II/solution.java
@@ -0,0 +1,21 @@
+// Author : Trusha Karandikar
+
+import java.util.Scanner;
+
+public class Solution
+ {
+    public static void main(String[] args) 
+    {
+        Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        double d = scanner.nextDouble();
+        scanner.nextLine();
+        String s = scanner.nextLine();
+
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+}
