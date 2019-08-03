31df89790ad1d0d63b36d4d8221871d4b3b0b9a9
trushakarandikar <trushakarandikar@gmail.com>
2019-08-03 12:24:30 +0530

    Create solution.java

---
+++ Introduction/Java Stdin and Stdout I/solution.java
@@ -0,0 +1,22 @@
+// Author : Trusha Karandikar
+
+import java.util.*;
+
+public class Solution 
+{
+    public static void main(String[] args) 
+    {
+        //read and save integers
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+        int c = scanner.nextInt();
+
+        //print integers
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+        scanner.close();
+    }
+}
